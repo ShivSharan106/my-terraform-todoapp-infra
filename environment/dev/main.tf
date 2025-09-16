94a8d93f179d09a0b350fb9efe109ab03609d795
Shiv Pandey <shiv8957999443@gmail.com>
2025-09-16 22:42:25 +0530

    all code

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -8,3 +8,23 @@ module "stgs_module" {
   stgs_map   = var.stgs_details
   depends_on = [module.rgs_module]
 }
+module "vnets_module" {
+  source     = "../../module/Vnet"
+  vnets_map  = var.vnets_details
+  depends_on = [module.rgs_module]
+}
+module "subs_module" {
+  source     = "../../module/subnet"
+  subs_map   = var.subs_details
+  depends_on = [module.rgs_module, module.vnets_module]
+}
+module "vms_module" {
+  source  = "../../module/vm"
+  vms_map = var.vm_details
+  depends_on = [ module.rgs_module,module.vnets_module,module.subs_module ]
+}
+module "pip_module" {
+  source = "../../module/public_ip"
+  pip_map = var.pip_details
+  depends_on = [ module.rgs_module ]
+}
